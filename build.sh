9f684c5136c7ff303a0e1496a9738e40f83a2991
Robin Kelley <rwilkerson@atu.edu>
2026-04-01 16:37:38 -0500

    Add auto superuser creation

--- build.sh
+++ build.sh
@@ -4,3 +4,10 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='lulu').exists():
+    User.objects.create_superuser('lulu', 'lulu@example.com', 'Password123!')
+END
